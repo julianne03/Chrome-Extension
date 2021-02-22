602f3071daa5774713b72aa888122342196d4663
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-02-22 14:18:46 +0900

    #2-4. CLOCK design and code modify

--- clock.js
+++ clock.js
@@ -1,28 +1,13 @@
-const clockBtn = document.querySelector("#clock");
-const wrapper = document.querySelector(".wrapper");
-
-const div = document.createElement("div");
-const span = document.createElement("span");
-const todayDiv = document.createElement("div");
-const todaySpan = document.createElement("span");
-
 function showClock() {
-
-    div.classList.add("js-clock-container");
-    span.classList.add("js-clock");
-    todayDiv.classList.add("js-today-container");
-    todaySpan.classList.add("js-today");
-
-    div.appendChild(span);
-    wrapper.appendChild(div);
-    todayDiv.appendChild(todaySpan);
-    wrapper.appendChild(todayDiv);
-
     getTime();
     setInterval(getTime, 1000);
 }
 
 function getTime() {
+
+    const date_text = document.querySelector(".date-text"),
+        time_text = document.querySelector(".time-text");
+
     const date = new Date();
     const seconds = date.getSeconds();
     const minutes = date.getMinutes();
@@ -35,18 +20,17 @@ function getTime() {
     const weekList = ['Sun','Mon','Tue','Wed','Thr','Fri','Sat'];
     const dayname = weekList[date.getDay()];
 
-    span.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    time_text.innerText = `${hours < 10 ? `0${hours}` : hours}:${
         minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds} ${ampm}`;
-    todaySpan.innerText = `${year}-${month}-${day}, ${dayname}`;    
+    date_text.innerText = `${year}-${month}-${day}, ${dayname}`;    
 }
 
 function handleClick() {
-    wrapper.innerHTML = "";
     showClock();
 }
 
 function init() {
     showClock();
-    clockBtn.addEventListener("click", handleClick);
+    clock.addEventListener("click", handleClick);
 }
 init();
