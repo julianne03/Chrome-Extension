344de9d1a9f83bd9b9f754f3d905035c1783ddce
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-02-23 13:39:16 +0900

    #2-9. make dynamic d-day function!

--- d_day.js
+++ d_day.js
@@ -1,14 +1,13 @@
-const dDayBtn = document.querySelector("#d-day"),
-    d_day_container = document.createElement("div");
-    form = document.createElement("form"),
-    input_event = document.createElement("input"),
-    input_date = document.createElement("input"),
-    date_container = document.createElement("div"),
-    days_span = document.createElement("span");
-
 const EVENT = "event";
 const DATE = "date";
-const SHOWING = "showing";
+
+const d_day_container = document.querySelector(".js-d-day-container"),
+    print_d_day = document.querySelector(".print-d-day"),
+    span_event = document.querySelector(".event-text"),
+    span_days = document.querySelector("#days"),
+    span_hours = document.querySelector("#hours"),
+    span_minutes = document.querySelector("#minutes"),
+    span_seconds = document.querySelector("#seconds");
 
 function formatTime(time) {
     return  Math.abs(time) < 10 ? (`0${Math.abs(time)}`) : Math.abs(time);
@@ -21,89 +20,67 @@ function getDday() {
     const d_day = new Date(parseInt(date_list[0]), parseInt(date_list[1]) - 1, parseInt(date_list[2]));
     const totalSeconds = (d_day - today) / 1000;
 
-
     const seconds = Math.floor(totalSeconds) % 60;
     const minutes = Math.floor(totalSeconds / 60) % 60;
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const days = Math.floor(totalSeconds / 3600 / 24);
     
-    days_span.innerText = `${formatTime(days)} ${formatTime(hours)} ${formatTime(minutes)} ${formatTime(seconds)}`;
+    span_days.innerText = `${formatTime(days)}`;
+    span_hours.innerText = `${formatTime(hours)}`;
+    span_minutes.innerText = `${formatTime(minutes)}`;
+    span_seconds.innerText = `${formatTime(seconds)}`;
 
 }
 
-function printDday(event) {
-    form.innerHTML = "";
-    wrapper.innerHTML = "";
-    const span = document.createElement("span");
-    span.innerText = `D - ${event}`;
-    d_day_container.appendChild(span);
+function printDday(event, date) {
+    const form = document.querySelector(".js-d-day-form");
+    form.style.display = 'none';
+    print_d_day.style.display = 'block';
+    span_event.innerText = `D - ${event}`;
     getDday();
-    date_container.appendChild(days_span);
-    d_day_container.appendChild(date_container);
-    wrapper.appendChild(d_day_container);
-
     setInterval(getDday, 1000);
 }
 
-function saveDate(date) {
-    localStorage.setItem(DATE, date);
-}
-
-function handleDateSubmit(event) {
-    event.preventDefault();
-    const currentDate = input_date.value;
-    const getEvent = localStorage.getItem(EVENT);
-    saveDate(currentDate);
-    printDday(getEvent);
-    
-} 
-
-function askDate() {
-    form.innerHTML = "";
-    wrapper.innerHTML = "";
-    form.appendChild(input_date);
-    d_day_container.appendChild(form);
-    wrapper.appendChild(d_day_container);
-    form.addEventListener("submit", handleDateSubmit);
-}
-
-function saveEvent(event) {
+function saveDday(event, date) {
     localStorage.setItem(EVENT, event);
+    localStorage.setItem(DATE, date);
 }
 
 function handleEventSubmit(event) {
     event.preventDefault();
+    const input_event = document.querySelector(".input-event");
+    const input_date = document.querySelector(".input-date");
     const currentEvent = input_event.value;
-    saveEvent(currentEvent);
-    askDate();
+    const currentDate = input_date.value;
+    saveDday(currentEvent, currentDate);
+    printDday(currentEvent, currentDate);
 }
 
-function askEvent() {
-    form.appendChild(input_event);
-    d_day_container.appendChild(form);
-    wrapper.appendChild(d_day_container);
-    form.addEventListener("submit", handleEventSubmit);
+function askDday() {
+    print_d_day.style.display = 'none';
+    const submit_btn = document.querySelector(".d-day-btn");
+    submit_btn.addEventListener("click", handleEventSubmit);
 }
 
-function loadEventDay() {
+function loadDday() {
     const event = localStorage.getItem(EVENT);
     const date = localStorage.getItem(DATE);
-    if (event === null) {
-        askEvent();
-    } else if (date === null) {
-        askDate();
+
+    if (event === null && date === null) {
+        askDday();
     } else {
-        printDday(event);
+        printDday(event, date);
     }
 }
 
 function handleClick() {
-    wrapper.innerHTML = "";
-    loadEventDay();
+    const page_style = document.querySelector("#page-style");
+    page_style.href = "d_day.css";
+    loadDday();
 }
 
 function init() {
-    dDayBtn.addEventListener('click', handleClick);
+    d_day.addEventListener('click', handleClick);
 }
 
 init();
